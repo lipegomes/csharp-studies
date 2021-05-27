06642d403d35dc414aae640c269253e23194712f
lipegomes <fgdl.py91@gmail.com>
2021-05-27 20:32:21 -0300

    Update

---
+++ MeuApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// Importações
+using System;
+
+// Namespace
+namespace MeuApp
+{
+    // Classe principal do programa
+    class Program
+    {
+        // Função principal do programa
+        static void Main(string[] args)
+        {
+
+            /*
+            Comentário
+            usando mais
+            de uma linha
+            */
+
+            var anime = "Kingdom";
+            int idade;
+            idade = 2000;
+            const int IDADE_MINIMA = 18;
+
+            Console.WriteLine(anime);
+            Console.WriteLine(idade);
+            Console.WriteLine(IDADE_MINIMA);
+        }
+    }
+}
