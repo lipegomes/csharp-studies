5bbff3c37f2408bffa65ccc96000c0c58eca4077
lipegomes <fgdl.py91@gmail.com>
2021-06-01 17:16:52 -0300

    Added OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipo de referência
+            // Armazena -> Endereço dos dados
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    // Classe
+    class Payment
+    {
+        // Propriedades
+        DateTime DueDate;
+
+        // Métodos
+        void Pay(){}
+    }
+}
