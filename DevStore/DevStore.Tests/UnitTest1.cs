635e328317fb8fc029ed18e9589b23f5ab7a7c79
lipegomes <fgdl.py91@gmail.com>
2021-06-18 20:44:51 -0300

    Implementando Notificações

--- DevStore/DevStore.Tests/UnitTest1.cs
+++ DevStore/DevStore.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using DevStore.Domain.StoreContext.Entities;
+using DevStore.Domain.StoreContext.ValueObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DevStore.Tests
@@ -9,15 +10,31 @@ namespace DevStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Customer(
-                "Filipe",
-                "de Lima",
-                "000000000001",
-                "foobar@foobar.com",
-                "01000000001",
-                "Avenue 100, number 7");
+            var name = new Name("Filipe", "Gomes");
+            var document = new Document("00000000000");
+            var email = new Email("test@foobar.com");
+            var c = new Customer(name, document, email, "01000000000");
+            var mouse = new Product("Mouse", "Rato", "image.png", 65.90M, 10);
+            var teclado = new Product("Teclado", "Teclado", "image.png", 256.99M, 10);
+            var impressora = new Product("Impressora", "Impressora", "image.png", 434.75M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "image.png", 498.90M, 10);
 
             var order = new Order(c);
+
+            // Realizei pedido
+            order.Place();
+
+            // Verificar se o pedido é valido
+            var valid = order.IsValid;
+
+            // Simular o pagamento
+            order.Pay();
+
+            // Simular o envio
+            order.Ship();
+
+            // Simular o cancelamento
+            order.Cancel();
         }
     }
 }
